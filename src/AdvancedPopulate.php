0610178747e6797f230b7c2ffa344fb28da8e454
Kenny1911 <o-muzyka@mail.ru>
2020-11-16 13:17:26 +0400

    refactor: AdvancedPopulate and AdvancedPopulateTest

--- src/AdvancedPopulate.php
+++ src/AdvancedPopulate.php
@@ -4,17 +4,17 @@ declare(strict_types=1);
 
 namespace Kenny1911\Populate;
 
-use Kenny1911\Populate\SettingsStorage\SettingsStorageInterface;
+use Kenny1911\Populate\Settings\SettingsInterface;
 
 class AdvancedPopulate implements PopulateInterface
 {
     /** @var PopulateInterface */
     protected $populate;
 
-    /** @var SettingsStorageInterface */
+    /** @var SettingsInterface */
     protected $settings;
 
-    public function __construct(PopulateInterface $populate, SettingsStorageInterface $settings)
+    public function __construct(PopulateInterface $populate, SettingsInterface $settings)
     {
         $this->populate = $populate;
         $this->settings = $settings;
@@ -24,12 +24,16 @@ class AdvancedPopulate implements PopulateInterface
         $src,
         $dest,
         ?array $properties = null,
-        array $ignoreProperties = [],
-        array $mapping = []
+        ?array $ignoreProperties = null,
+        ?array $mapping = null
     ): void
     {
-        $properties = $properties ?? $this->settings->getProperties($src, $dest);
-        // TODO get $ignoreProperties from settings
+        $properties = $properties ?? [];
+        $ignoreProperties = $ignoreProperties ?? [];
+        $mapping = $mapping ?? [];
+
+        $properties = $properties ?: $this->settings->getProperties($src, $dest);
+        $ignoreProperties = $ignoreProperties ?: $this->settings->getIgnoreProperties($src, $dest);
         $mapping = array_merge($this->settings->getMapping($src, $dest), $mapping);
 
         $this->populate->populate($src, $dest, $properties, $ignoreProperties, $mapping);
--- tests/AdvancedPopulateTest.php
+++ tests/AdvancedPopulateTest.php
@@ -7,75 +7,83 @@ namespace Kenny1911\Populate\Tests;
 use Kenny1911\Populate\AdvancedPopulate;
 use Kenny1911\Populate\ObjectAccessor\ObjectAccessor;
 use Kenny1911\Populate\Populate;
-use Kenny1911\Populate\SettingsStorage\SettingsStorage;
+use Kenny1911\Populate\Settings\Settings;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\PropertyAccess\PropertyAccess;
 use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
 
 class AdvancedPopulateTest extends TestCase
 {
-    private $src;
-
-    private $dest;
-
-    /** @var SettingsStorage */
-    private $settings;
-
-    /** @var AdvancedPopulate */
-    private $populate;
-
     public function testPopulate()
     {
-        $this->populate->populate($this->src, $this->dest);
+        $src = new Src(123, 456, 789);
+        $dest = new Dest();
 
-        $this->assertNull($this->dest->foo);
-        $this->assertNull($this->dest->getBar());
-        $this->assertNull($this->dest->getBaz());
+        $this->createPopulate()->populate($src, $dest);
+
+        $this->assertNull($dest->foo);
+        $this->assertNull($dest->getBar());
+        $this->assertNull($dest->getBaz());
     }
 
     public function testPopulateWithSettings()
     {
-        $this->settings->setProperties($this->src, $this->dest, ['public', 'private']);
-        $this->settings->setMapping($this->src, $this->dest, ['protected' => 'bar', 'private' => 'baz']);
+        $src = new Src(123, 456, 789);
+        $dest = new Dest();
 
-        $this->populate->populate($this->src, $this->dest);
+        $settings = [
+            [
+                'src' => Src::class,
+                'dest' => Dest::class,
+                'properties' => ['public', 'private'],
+                'mapping' => ['protected' => 'bar', 'private' => 'baz']
+            ]
+        ];
 
-        $this->assertNull($this->dest->foo);
-        $this->assertNull($this->dest->getBar());
-        $this->assertSame(789, $this->dest->getBaz());
+        $this->createPopulate($settings)->populate($src, $dest);
+
+        $this->assertNull($dest->foo);
+        $this->assertNull($dest->getBar());
+        $this->assertSame(789, $dest->getBaz());
     }
 
     public function testPopulateOverrideSettings()
     {
-        $this->settings->setProperties($this->src, $this->dest, ['public', 'private']);
-        $this->settings->setMapping($this->src, $this->dest, ['protected' => 'bar', 'private' => 'baz']);
+        $src = new Src(123, 456, 789);
+        $dest = new Dest();
+
+        $settings = [
+            [
+                'src' => Src::class,
+                'dest' => Dest::class,
+                'properties' => ['public', 'private'],
+                'mapping' => ['protected' => 'bar', 'private' => 'baz']
+            ]
+        ];
 
         $properties = ['public', 'protected'];
         $ignoreProperties = ['protected'];
         $mapping = ['public' => 'foo', 'protected' => 'protected'];
-        $this->populate->populate($this->src, $this->dest, $properties, $ignoreProperties, $mapping);
 
-        $this->assertSame(123, $this->dest->foo);
-        $this->assertNull($this->dest->getBar());
-        $this->assertNull($this->dest->getBaz());
+        $this->createPopulate($settings)->populate($src, $dest, $properties, $ignoreProperties, $mapping);
+
+        $this->assertSame(123, $dest->foo);
+        $this->assertNull($dest->getBar());
+        $this->assertNull($dest->getBaz());
 
 
     }
 
-    protected function setUp(): void
+    private function createPopulate(array $settings = []): AdvancedPopulate
     {
-        $this->src = new Src(123, 456, 789);
-        $this->dest = new Dest();
-
-        $this->settings = new SettingsStorage();
-        $this->populate = new AdvancedPopulate(
+        return new AdvancedPopulate(
             new Populate(
                 new ObjectAccessor(
                     PropertyAccess::createPropertyAccessor(),
                     new ReflectionExtractor()
                 )
             ),
-            $this->settings
+            new Settings($settings)
         );
     }
 }
